15d540025ef63f6c43b1679db482cd072661139c
JossWritesCode <joscelynrose@gmail.com>
2020-04-08 17:04:55 -0400

    made a room class

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,27 @@
 from room import Room
 
+# file i/o functions for historical results
+
+
+def load_results():
+    text_file = open("history.txt", "r")
+    history = text_file.read().split(",")
+    text_file.close()
+    return history
+
+
+def save_results(w, t, l):
+    text_file = open("history.txt", "w")
+    text_file.write(str(w) + "," + str(t) + "," + str(l))
+    text_file.close()
+
+
+# welcome message
+
+print("Welcome to my dungeon!")
+print("Please choose to continue...")
+
+
 # Declare all the rooms
 
 room = {
