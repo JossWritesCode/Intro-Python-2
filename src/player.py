54c5cd4d6a47031e24d38dfbc61192a2fabae03c
JossWritesCode <joscelynrose@gmail.com>
2020-04-09 19:43:11 -0400

    allowed player to enter name

--- src/player.py
+++ src/player.py
@@ -3,11 +3,14 @@
 
 
 class Player():
-    def __init__(self, name, current_room):
-        self.name = name
+    def __init__(self, current_room):
+        self.name = "Player 1"
         self.current_room = current_room
         self.current_room_info = None
         self.inventory = []
 
     def add_item(self, item):
         self.inventory.append(item)
+
+    def set_name(self, name):
+        self.name = name
